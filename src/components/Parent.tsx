ee6f27f6405507f783d20ef70afd67871ce4943e
FabioPin <fajos.pinheiro@hotmail.com>
2022-10-19 15:53:49 +0100

    ADDED SIBLING

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {IParentState} from './IParentState';
 
 import {Child} from './Child';
+import { Sibling } from "./Sibling";
 
 export class Parent extends React.Component<{}, IParentState> {
     constructor(props: any) {
@@ -22,7 +23,10 @@ export class Parent extends React.Component<{}, IParentState> {
 
     public render() {
         return (
-            <Child name={this.state.name} onChange={this.changeName} />
+            <div>
+                <Child name={this.state.name} onChange={this.changeName} />
+                <Sibling />
+            </div>
         )
     }
 }
